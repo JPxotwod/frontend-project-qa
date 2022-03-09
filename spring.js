f74878d3ea8e0052a5274bba0552f52cb0867b5f
Practice <josephp123@sky.com>
2022-03-09 14:16:45 +0000

    Front End development cont.

--- spring.js
+++ spring.js
@@ -34,9 +34,11 @@ for (let d=0;d<returnedData.length;d++){
     <td>`+returnedData[d].team+`</td>
     <td>`+returnedData[d].position+`</td>
     </tr>`;
-    console.log(allTable);
-    document.getElementById("player").innerHTML = allTable;
 }
+
+console.log(allTable);
+document.getElementById("player").innerHTML = allTable;
+
 }
 
 let getById = async () => {
@@ -54,7 +56,7 @@ let getById = async () => {
 }
 
 let getByName = async () => {
-    let i = document.getElementById("name").value;
+    let i = document.getElementById("playerName").value;
     let response = await fetch ('http://localhost:8080/getByName/' +i);
     if (response.status !==200) {
         throw new Error("Request Failed");
@@ -64,10 +66,31 @@ let getByName = async () => {
     let jsonData = await response.json();
     console.log(jsonData);
     return jsonData;
+    // let allTable =
+    // `<tr>
+    // <th>ID</th>
+    // <th>Name</th>
+    // <th>Number</th>
+    // <th>Team</th>
+    // <th>Position</th>
+    // </tr>`;
+    // for (let d=0;d<jsonData.length;d++){
+    //     allTable += `<tr>
+    //     <td>`+jsonData[d].id+`</td>
+    //     <td>`+jsonData[d].name+`</td>
+    //     <td>`+jsonData[d].number+`</td>
+    //     <td>`+jsonData[d].team+`</td>
+    //     <td>`+jsonData[d].position+`</td>
+    //     </tr>`;
+    // }
+
+    // console.log(allTable);
+    // document.getElementById("player").innerHTML = allTable;
   
 }
 
 let getByNumber = async () => {
+    let i = parseInt(document.getElementById("playerNumber").value);
     let response = await fetch ('http://localhost:8080/getByNumber/' +i);
     if (response.status !==200) {
         throw new Error("Request Failed");
@@ -104,6 +127,28 @@ function createPlayer() {
     });
 }
 
+let replaceData = async () => {
+    let i = document.getElementById("replaceId").value;
+    const data = {
+        name: document.getElementById("rName").value,
+        number: document.getElementById("rNumber").value,
+        team: document.getElementById("rTeam").value,
+        position: document.getElementById("rPosition").value,
+    };
+
+    fetch("http://localhost:8080/replace" +i, {
+        method: "PUT",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)})
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(error => error);
+    }
+
+
 
 function removePlayer(id) {
     fetch("http://localhost:8080/remove/" + id, {
@@ -114,4 +159,46 @@ function removePlayer(id) {
     }).catch(error => console.error(error));
 }
 
+function showHide(obj) {
+    for(x=1;x<=1;x++) {
+        if (x == obj) {
+            document.getElementById('m' +x).style.display='block';
+        } else {
+            document.getElementById('m'+x).style.display='none';
+        }
+    }
+}
+function openF(obj) {
+    for(let x=1;x<=4;x++) {
+        if (x == obj) {
+            document.getElementById('f' +x).style.display='block';
+        }
 
+    }
+}
+function closeF(obj) {
+    for(let x=1;x<=4;x++) {
+        if (x == obj) {
+            document.getElementById('f' +x).style.display='none';
+        }
+    }
+}
+
+function showID(obj) {
+    if (obj.style.display=="none") {
+        obj.style.display="block"
+    }
+}
+
+
+function showMenu() {
+    
+    let obj=document.getElementById("m1");
+    if (obj.style.display=="none") {
+        obj.style.display="block"
+    }
+    else {
+        obj.style.display="none";
+    }
+    
+}
